ce28b5f21da5f2b0db3620922741779d497d049a
JShad30 <jimkarts@hotmail.co.uk>
2019-07-31 09:47:33 +0100

    Migrations made

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
+	intro = models.CharField(max_length=250)
 	content = models.TextField()
+	image = models.ImageField(default='blog_default.jpg', upload_to='blog_images')
+
+	def save(self):
+		super().save()
+
+		img = Image.open(self.image.path)
+
+		if img.height > 600 or img.width > 1200:
+			output_size = (600,1200)
+			img.header(output_size)
+			img.save(self.image.path)
+
 	"""Ensure the timezone.now doesn't have parenthesis(). That would run the function at that moment"""
 	date_posted = models.DateTimeField(default=timezone.now)
 	"""CASCADE - tells django that if a user is deleted all their posts will be deleted too"""
@@ -18,5 +32,10 @@ class Post(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+
+
+	
 
 
