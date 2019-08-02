be928fc2746bb2d0d295e0af9dbe5596b4a06074
JShad30 <jimkarts@hotmail.co.uk>
2019-08-02 14:53:52 +0100

    Update home app

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home, about, terms, privacy, contact
 
 urlpatterns = [
+    path('', home, name='home'),
     path('home/', home, name='home'),
     path('about/', about, name='about'),
     path('terms/', terms, name='terms'),
