9c04017e328ec7a26b5bc41541029ac57c205d47
JShad30 <jimkarts@hotmail.co.uk>
2019-08-02 16:36:22 +0100

    Contact form and requirements added

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django import forms
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def home(request):
@@ -14,4 +18,8 @@ def privacy(request):
     return render(request, 'home/privacy.html')
 
 def contact(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+
+        send_mail('Contact Form', message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER], fail_silently=FALSE)
     return render(request, 'home/contact.html')
