9b1992aa1194923cb088bea31b4263542fbe78af
JShad30 <jimkarts@hotmail.co.uk>
2019-08-04 10:22:18 +0100

    Updating blog to add image

--- blog/models.py
+++ blog/models.py
@@ -7,19 +7,22 @@ from PIL import Image
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	image = models.ImageField(default='blog-default.jpg', upload_to='blog_images')
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
 
+	def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        blog_image = Image.open(self.image.path)
+
+        if blog_image.height > 1200 or blog_image.width > 700:
+            blog_output_size = (1200, 700)
+            blog_image.thumbnail(blog_output_size)
+            blog_image.save(self.image.path)
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
-
-
-
-
-	
-
-
