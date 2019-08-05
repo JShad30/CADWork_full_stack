190599be83369233a7270057012a0107bcc7af52
JShad30 <jimkarts@hotmail.co.uk>
2019-08-06 00:22:53 +0100

    Worked on blog section, layout and now able to add images

--- blog/models.py
+++ blog/models.py
@@ -6,23 +6,24 @@ from PIL import Image
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
+	intro = models.CharField(max_length=250)
 	content = models.TextField()
-	image = models.ImageField(default='blog-default.jpg', upload_to='blog_images')
+	blog_image = models.ImageField(default='blog-default.jpg', upload_to='blog_images')
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
 
-	def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        blog_image = Image.open(self.image.path)
-
-        if blog_image.height > 1200 or blog_image.width > 700:
-            blog_output_size = (1200, 700)
-            blog_image.thumbnail(blog_output_size)
-            blog_image.save(self.image.path)
-
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
+
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
+
+		blog_image = Image.open(self.blog_image)
+
+		if blog_image.width > 1200 or blog_image.height > 700:
+			output_size = (1200, 700)
+			blog_image.resize(output_size)
+			blog_image.save(self.blog_image.path)
