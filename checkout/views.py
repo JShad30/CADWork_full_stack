fc6eaf347cb8552e6fbba53166547940438188e6
JShad30 <jimkarts@hotmail.co.uk>
2019-08-29 12:13:56 +0100

    Updated views.py file with logging

--- checkout/views.py
+++ checkout/views.py
@@ -14,12 +14,15 @@ stripe.api_key = settings.STRIPE_SECRET
 """Home view for the shop checkout"""
 @login_required()
 def checkout(request):
+    logging.info('1. Entering Checkout')
     #Two different forms created for the user information and payment form
     if request.method == 'POST':
+        logging.info('2. Entering Post')
         o_form = OrderForm(request.POST)
         p_form = MakePaymentForm(request.POST)
         #Check that both forms are valid
         if o_form.is_valid() and p_form.is_valid():
+            logging.info('3. Entering Valid')
             order = o_form.save(commit=False)
             order.date = timezone.now()
             order.save()
@@ -37,6 +40,7 @@ def checkout(request):
                 order_line_item.save()
 
             try:
+                logging.info('4. Entering Stripe')
                 logging.info(str(int(total * 100)))
                 customer = stripe.Charge.create(
                     amount = int(total * 100), 
@@ -45,14 +49,17 @@ def checkout(request):
                     card = p_form.cleaned_data['stripe_id']
                 )
             except stripe.error.CardError:
+                logging.info('4b. Stripe Failed')
                 messages.error(request, 'Your card has been declined')
 
             #If statement to determine which message to print
             if customer.paid:
+                logging.info('5. Success (paid)')
                 messages.error(request, 'Thanks for your payment, it has been successfully processed')
                 request.session['cart'] = {}
                 return redirect('shop-home')
             else:
+                logging.info('5b. Failed')
                 messages.error(request, 'We were unable to accept your payment')
 
         #Message to print of forms are not valid
