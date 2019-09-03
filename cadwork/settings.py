9d01a3fec8a9507057ad755154d08bdfec2d57ae
JShad30 <jimkarts@hotmail.co.uk>
2019-09-03 23:49:05 +0100

    Updated databases on settings

--- cadwork/settings.py
+++ cadwork/settings.py
@@ -94,15 +94,16 @@ WSGI_APPLICATION = 'cadwork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#Commented out database that was used during development
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print('Database URL is not found. Using SQLite')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
