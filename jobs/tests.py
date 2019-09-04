b337d4656c4032d3015550d8799cde04fd151230
JShad30 <jimkarts@hotmail.co.uk>
2019-09-04 10:42:04 +0100

    Updated jobs test

--- jobs/tests.py
+++ jobs/tests.py
@@ -26,7 +26,6 @@ class TestJobsModels(TestCase):
         self.assertEqual(job.job_description, 'Content to go into the textfield')
         self.assertEqual(job.job_location_town, 'Town')
         self.assertEqual(job.job_location_county, 'County')
-        self.assertEqual(job.image, '/job_images/image.jpg')
 
     def test_job_file(self):
         job_file = JobFileUpload(file_name='Test file', file_price=2.00, uploaded_file='test.pdf')
