b603091be72c876dd8f1591afc83f9d845b0441f
JShad30 <jimkarts@hotmail.co.uk>
2019-09-04 12:43:03 +0100

    Updated tests

--- blog/tests.py
+++ blog/tests.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 #Imports for app testing
 from .apps import BlogConfig
 
-#Imports for model and views testing
+#Imports for views testing
 from .models import Post, PostComment
 
 # Create your tests here.
@@ -15,35 +15,7 @@ class TestBlogConfig(TestCase):
     def test_blog_app(self):
         self.assertEqual("blog", BlogConfig.name)
         self.assertEqual("blog", apps.get_app_config("blog").name)
-
-#Testing a model for the blog
-class TestBlogModels(TestCase):
-
-    def test_create_post(self):
-        User.objects.create_user(
-            username='testuser',
-            email='testuser@email.com',
-            password='passwordtest')
-        self.client.login(username='testuser', password='passwordtest')
-        post = Post(title='Blog post', intro='Basic blog post content intro', content='Content to go into the textfield')
-        post.save()
-        self.assertEqual(post.author.username, 'testuser')
-        self.assertEqual(post.title, 'Blog post')
-        self.assertEqual(post.intro, 'Basic blog post content intro')
-        self.assertEqual(post.content, 'Content to go into the textfield')
-
-    def test_post_comment(self):
-        User.objects.create_user(
-            username='testuser',
-            email='testuser@email.com',
-            password='passwordtest')
-        self.client.login(username='testuser', password='passwordtest')
-        post_comment = PostComment(comment='Test comment', post_id='1')
-        post_comment.save() 
-        self.assertEqual(post_comment.post_id, '1')
-        self.assertEqual(post_comment.comment, 'Test comment')
-        self.assertEqual(post_comment.author.username, 'testuser')
-
+        
 #Testing the views
 class TestBlogViews(TestCase):
     def test_get_home_blog_page(self):
--- jobs/tests.py
+++ jobs/tests.py
@@ -4,7 +4,7 @@ from django.apps import apps
 #Imports for app testing
 from .apps import JobsConfig
 
-#Imports for model and views testing
+#Imports for views testing
 from .models import Job, JobFileUpload, JobComment
 
 # Create your tests here.
@@ -14,28 +14,6 @@ class TestJobConfig(TestCase):
     def test_jobs_app(self):
         self.assertEqual("jobs", JobsConfig.name)
         self.assertEqual("jobs", apps.get_app_config("jobs").name)
-
-#Testing a model for the blog
-class TestJobsModels(TestCase):
-
-    def test_create_job(self):
-        job = Job(job_name='New Job', job_overview='Basic job content intro', job_description='Content to go into the textfield', job_location_town='Town', job_location_county='County')
-        job.save()
-        self.assertEqual(job.job_name, 'New Job')
-        self.assertEqual(job.job_overview, 'Basic job content intro')
-        self.assertEqual(job.job_description, 'Content to go into the textfield')
-        self.assertEqual(job.job_location_town, 'Town')
-        self.assertEqual(job.job_location_county, 'County')
-
-    def test_job_file(self):
-        job_file = JobFileUpload(file_name='Test file', file_price=2.00, uploaded_file='test.pdf')
-        self.assertEqual(job_file.file_name, 'Test file')
-        self.assertEqual(job_file.file_price, 2.00)
-        self.assertEqual(job_file.uploaded_file, 'test.pdf')
-
-    def test_job_comment(self):
-        job_comment = JobComment(comment='Test comment')
-        self.assertEqual(job_comment.comment, 'Test comment')
 
 #Testing the views
 class TestJobViews(TestCase):
