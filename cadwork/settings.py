00c8e2a3c773cbd1d14703c93d350a6185e2dc38
JShad30 <jimkarts@hotmail.co.uk>
2019-09-06 15:24:44 +0100

    Update email variables in settings

--- cadwork/settings.py
+++ cadwork/settings.py
@@ -175,8 +175,9 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_USER = os.environ.get('EMAIL_USER')
+EMAIL_PASS = os.environ.get('EMAIL_PASS')
+DEFAULT_FROM_EMAIL = EMAIL_USER
 
 LOGGING = {
     'version': 1,
