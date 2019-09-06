2ba606b6877b0edcf86b337d60cdbc9c397604fd
JShad30 <jimkarts@hotmail.co.uk>
2019-09-06 15:57:08 +0100

    Update settings for email environment variables

--- cadwork/settings.py
+++ cadwork/settings.py
@@ -177,7 +177,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
-SMTP_ENABLED = True
 
 LOGGING = {
     'version': 1,
