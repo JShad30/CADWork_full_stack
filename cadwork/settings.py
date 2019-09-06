76b5e186a75517d747d7254f74044f06a64d38fd
JShad30 <jimkarts@hotmail.co.uk>
2019-09-06 16:00:49 +0100

    Update settings file

--- cadwork/settings.py
+++ cadwork/settings.py
@@ -172,11 +172,13 @@ LOGIN_REDIRECT_URL = 'profile'
 
 LOGIN_URL = 'login'
 
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGGING = {
     'version': 1,
